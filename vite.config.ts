e58e32a2bd42df8c94a29e6210ca0d98b27215d0
Jang Haemin <io@jhaemin.com>
2022-10-15 18:38:01 +0900

    Remove base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/dynamic-island/',
   build: {
-    outDir: './pages'
-  }
+    outDir: './pages',
+  },
 })
